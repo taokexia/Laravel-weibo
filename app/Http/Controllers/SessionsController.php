692f238ce42cc21508cdf0ec2d7a9facbacbf4ac
taokexia <694198149@qq.com>
2020-07-28 16:59:19 +0800

    Add: 用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -31,4 +31,12 @@ class SessionsController extends Controller
  
         return;
     }
+
+    // 退出
+    public function destory()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出!');
+        return redirect('login');
+    }
 }
